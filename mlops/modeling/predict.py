355b04f4716499855a88e552081f39e4208354b9
loadi <53373154+loadi@users.noreply.github.com>
2025-01-02 00:59:55 +0300

    add func to calc model accuracy

---
+++ mlops/modeling/predict.py
@@ -0,0 +1,10 @@
+import torch
+
+
+def get_model_accuracy(model, test_dataloader):
+    correct = 0
+
+    for batch_idx, (x, y) in enumerate(test_dataloader):
+        y_pred = model.predict(x)
+        correct += (y_pred.argmax(1) == y).type(torch.float).sum().item()
+    return correct / len(test_dataloader.dataset)
